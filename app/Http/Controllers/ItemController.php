8e98536c43ed68cd8439ac6cd15385d89abace2c
ryo-takehira <145681998+ryo-takehira@users.noreply.github.com>
2024-03-19 12:11:26 +0900

    ItemController.php の更新 画像圧縮修正

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -208,7 +208,7 @@ class ItemController extends Controller
                 // 希望するドライバーで新しいマネージャーでファイルを読み取る
                 $img = $manager->read($image_file);
                 // サイズ変更で圧縮
-                $img->resize(height: 375px, width: 500px);
+                $img->resize(375,null);
 
                 // ピクセレーション効果で圧縮
                 $img = $img->pixelate(0.5);
